b17c8f2f9db96b7384b1ad0ba924067cc017d1a7
Korhan Akcura <korhan.akcura@sgdigital.com>
2019-02-09 20:50:16 -0500

    Initial version.

---
+++ server.py
@@ -0,0 +1,27 @@
+# MoodTrackr : Server to track your mood.
+# Authors :  Korhan Akcura
+
+import json
+import requests
+import logging
+
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__, static_url_path='')
+
+@app.route("/", methods=['GET', 'POST'])
+def root(): 
+	return app.send_static_file('index.html')
+
+@app.route('/analize', methods=['POST'])
+def analize():
+	return "You are happy!" 
+
+if __name__ == '__main__':
+	handler = RotatingFileHandler('server.log', maxBytes=10000, backupCount=1)
+	handler.setLevel(logging.INFO)
+	app.logger.addHandler(handler)
+	app.run(host="127.0.0.1",port=5000)
+
